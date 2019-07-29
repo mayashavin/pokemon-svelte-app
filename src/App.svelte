3e60d58f889585d420f94b6ac7a9dfe5e4d552fa
Maya Shavin <maya@cloudinary.com>
2019-07-29 10:28:25 +0300

    init

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+	import PokemonCard from './atoms/PokemonCard.svelte';
+	import database from './data/pokemons.js';
+
+	const pokemons = Object.keys(database.pokemons).map(key => (database.pokemons[key]));
+	let name = "Maya"
+</script>
+
+<style>
+	h1 {
+		color: purple;
+		text-align: center;
+	}
+	div {
+		display: flex;
+		flex-wrap: wrap;
+	}
+</style>
+
+<h1>Pokemons Svelte Test</h1>
+<div class="grid-container">
+	{#each pokemons as pokemon (pokemon.publicId)}
+	<PokemonCard card={pokemon}/>
+	{/each}
+</div>
---
+++ src/atoms/PokemonCard.svelte
@@ -0,0 +1,38 @@
+<script>
+  export let card;
+</script>
+<style>
+  .item {
+    border: 1px solid lightgray;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    padding: 5px;
+    margin-right:10px;
+    margin-bottom: 10px;
+    width: 220px;
+    box-shadow: 0 1px 2px rgba(0,0,0,.2);
+  }
+
+  .item:hover {
+    background-color: #d3d3d363;
+    cursor: pointer;
+  }
+
+  h3 {
+    margin: 0;
+    padding: 0.8rem 0;
+    border-top: 1px solid lightgray;
+    text-align: center;
+  }
+
+  img {
+    width: 200px;
+    height: auto;
+  }
+
+</style>
+<div class="item">
+  <img src={card.url} alt={card.title}/>
+  <h3>{card.title}</h3>
+</div>
